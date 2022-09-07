f21ded5a2ce1f9b0cf107e4ab46a7ced5b71b932
SaulRmzObr <saul.ramirez.obregon@gmail.com>
2022-09-06 22:19:30 -0500

    Se crean clases DTO y su configuracion de mapeo para usarlas para insertar datos en bd

---
+++ EFCorePeliculas/DTOs/CineCreacionDTO.cs
@@ -0,0 +1,15 @@
+﻿using EFCorePeliculas.Entidades;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFCorePeliculas.DTOs
+{
+    public class CineCreacionDTO
+    {
+        [Required]
+        public string sNombre { get; set; }
+        public double dLatitud { get; set; }
+        public double dLongitud { get; set; }
+        public CineOfertaCreacionDTO CineOferta { get; set; }
+        public SalaDeCineCreacionDTO[] SalasDeCine { get; set; }
+    }
+}
---
+++ EFCorePeliculas/DTOs/CineOfertaCreacionDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFCorePeliculas.DTOs
+{
+    public class CineOfertaCreacionDTO
+    {
+        public DateTime dFechaInicio { get; set; }
+        public DateTime dFechaFin { get; set; }
+        [Range(1,100)]
+        public decimal dPorcentajeDescuento { get; set; }
+    }
+}
