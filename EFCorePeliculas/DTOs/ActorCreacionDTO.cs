662e94c605044e645ec0da705017578e3511cd76
SaulRmzObr <saul.ramirez.obregon@gmail.com>
2022-09-08 23:04:35 -0500

    Creacion de clases DTO para registrar y actualizar registros

---
+++ EFCorePeliculas/DTOs/ActorCreacionDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCorePeliculas.DTOs
+{
+    public class ActorCreacionDTO
+    {
+        public string sNombre { get; set; }
+        public string sBiografia { get; set; }
+        public DateTime? dFechaNacimiento { get; set; }
+    }
+}
---
+++ EFCorePeliculas/DTOs/PeliculaCreacionDTO.cs
@@ -0,0 +1,14 @@
+﻿using EFCorePeliculas.Entidades;
+
+namespace EFCorePeliculas.DTOs
+{
+    public class PeliculaCreacionDTO
+    {
+        public string sTitulo { get; set; }
+        public bool bSiNoCartelera { get; set; }
+        public DateTime dFechaEstreno { get; set; }
+        public List<int> Generos { get; set; }
+        public List<int> SalasDeCine { get; set; }
+        public List<PeliculaActorCreacionDTO> PeliculasActores { get; set; }
+    }
+}
