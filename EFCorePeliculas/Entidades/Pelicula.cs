225de87237d2c4f06770ffc8bb742b9989893454
SaulRmzObr <saul.ramirez.obregon@gmail.com>
2022-09-08 23:07:30 -0500

    Se cambia tipo HashSet a List a propiedades para poder iterarlas

--- EFCorePeliculas/Entidades/Pelicula.cs
+++ EFCorePeliculas/Entidades/Pelicula.cs
@@ -9,7 +9,7 @@
         //[Unicode(false)]
         public string sPosterUrl { get; set; }
         public List<Genero> Generos { get; set; }
-        public HashSet<SalaDeCine> SalasDeCine { get; set; }
-        public HashSet<PeliculaActor> PeliculasActores { get; set; }
+        public List<SalaDeCine> SalasDeCine { get; set; }
+        public List<PeliculaActor> PeliculasActores { get; set; }
     }
 }
