8ff3d29130f8a18c03b16f091313c8a838e9ac90
SaulRmzObr <saul.ramirez.obregon@gmail.com>
2022-09-08 23:05:22 -0500

    Se agrega campo en la entidad para crear mapeo flexible de una propiedad

--- EFCorePeliculas/Entidades/Actor.cs
+++ EFCorePeliculas/Entidades/Actor.cs
@@ -3,7 +3,20 @@
     public class Actor
     {
         public int idActor { get; set; }
-        public string sNombre { get; set; }
+        private string _sNombre;
+        public string sNombre 
+        {
+            get { return _sNombre; }
+            set
+            {
+                _sNombre = String.Join(' ', 
+                    value.Split(' ')
+                    .Select(letra => 
+                    string.Format("{0}{1}", 
+                    letra[0].ToString().ToUpper(), 
+                    letra.Substring(1).ToLower())).ToArray());
+            }
+        }
         public string sBiografia { get; set; }
         //[Column(TypeName ="Date")]
         public DateTime? dFechaNacimiento { get; set; }
